8e77aa3b7e8192db8ce2b292520d4d879d556068
Ivan Domenzain <ivand@chalmers.se>
2018-08-22 15:02:24 +0200

    updated excercise2 files

--- CHASSY_ecModels/Excercise2.md
+++ CHASSY_ecModels/Excercise2.md
@@ -15,11 +15,28 @@ Now that we have explored the whole model structure it is time to start to use t
 
    * d) Was there any limiting compound in the simulation medium?
 
-1. Plot the growth rate vs glucose uptake rate in the range 0 - 100 mol/gDCW/h. Any comments about the results?
+1. Plot the growth rate vs glucose uptake rate on the range 0 - 20 mmol/gDCW/h. Any comments about the results?
 
-1. 
+As you just saw, imposing proper constraints is crucial when running FBA. A mathematically feasible simulation doesn't necesarilly imply biological relevance!
 
+Properly constrained GEMs allow to take a look to the metabolic network in action subject to specific environmental  conditions. Microbial growth on different carbon sources can be easily explored with the use of a GEM. Now you are going to explore the metabolic differences for yeast growing on limited carbon conditions using two different substrates. 
 
+4. Run FBA simulations for carbon limited conditions using:
+
+    * a) Glucose as a carbon source with an upper bound of 1 mmol/gDCW h  for its **uptake** reaction
+
+    * b) Glycerol with an upper bound of 1 mmol/gDCW h for its **uptake** reaction 
+
+    * c) Display the reactions which relative flux difference is, at least, 50% with respect to the glucose conditions and with an absolute difference higher than 0.5 mmol/gDCW h.
+
+    * d) Get the reactios with significantly changing fluxes in which ATP is involved.
+
+    * e) Take a look to this results and discuss with a partner what do you observe from a metabolic point of view, you can use the EXCEL file for interpreting the results.
+
+By now, we have a modified model for the production of octanoic acid, its production performance for a wide range of conditions can be analysed through FBA.
+
+5. Get a octanoic acid yield vs biomass theoretical yield plot (in terms of mass).
+    * a) According to this result would it be attractive to explore octanoic acid production with *S. cerevisiae* as a cell factory?
 
  
 
--- CHASSY_ecModels/scripts/Exercise2.m
+++ CHASSY_ecModels/scripts/Exercise2.m
@@ -43,7 +43,7 @@ load('../models/GEMs/octAcidModel.mat')
 % 3) Plot growth rate vs GUR
     gRates  = [];
     GURates = [];    
-    for i=1:50
+    for i=1:20
         %Set new lb for glucose uptake at every iteration
         GUR = i;
         tempModel = setParam(octModel,'lb','r_1714',-GUR);
@@ -76,27 +76,28 @@ load('../models/GEMs/octAcidModel.mat')
     %What if you are interested in how metabolism changes between the two 
     %conditions. followChanged takes two flux distributions and lets you
     %select which reactions to print. Here we show reactions that differ with
-    %more than 50%, has a flux higher than 0.5 mmol/gDW/h and an absolute
+    %more than 50%, have a flux higher than 0.5 mmol/gDW/h and an absolute
     %difference higher than 0.5 mmol/gDW/h.
     followChanged(tempModel,solGluc.x,solGly.x, 50, 0.5, 0.5);
     fprintf('\n')
     %Say that we are particularly interested in how ATP metabolism
-    %changes. Then we can show only such reaction by writing,
+    %changes. Then we can show its related reactions by writing,
     followChanged(tempModel,solGluc.x,solGly.x, 30, 0.4, 0.4,{'ATP'});
     fprintf('\n')
 
 % 5) Get a yield vs gRate plot for octanoic acid   
-    BioYield = [];
-    Octyield = [];
+    BioYield    = [];
+    Octyield    = [];
     ocAcidIndex = find(strcmpi(octModel.rxnNames,'octanoic acid exchange'));
-    iterations = 20;
+    iterations  = 20;
+    miuMax      = 0.41;
     for i=1:iterations+1
         % Set the objective function to the added octanoic acid exchange rxn
         tempModel = setParam(octModel,'obj','octanoic acid exchange',1);
         % Set a minimal glucose media
-         tempModel = setMinimalMedia(tempModel,'r_1714',-5);
+         tempModel = setMinimalMedia(tempModel,'r_1714',-4);
         % Fix dilution rate at every iteration
-        Drate     = (0.4)*(i-1)/iterations;
+        Drate     = miuMax*(i-1)/iterations;
         tempModel = setParam(tempModel,'eq','r_4041',Drate);
         sol       = solveLP(tempModel,1);
         if ~isempty(sol.f)
