d9a6c86bd6bab6cfce496aa07b825426cfa34675
Ivan Domenzain <ivand@chalmers.se>
2018-08-22 09:31:11 +0200

    fix: Excercise1 finished and tested

--- CHASSY_ecModels/Excercise1.md
+++ CHASSY_ecModels/Excercise1.md
@@ -23,7 +23,14 @@ As a first approach to GEMs we are going to inspect the structure and element in
 1. Now you are ready for inspecting the model `.mat` file (MATLAB structure). Take your time for looking at its multiple fields: model.mets, model.rxns, model.S, model.grRules, etc. You can find all the fields that are supported by the `RAVEN` toolbox by entering `help importModel` in your MATLAB command line. 
     
     For illustrating some of the advantages of dealing with a GEM in a MATLAB structure instead of an EXCEL model, repeat the tasks 2a-e (go to the excercise script).
-1. If a reaction is properly removed from the model, which fields sj be affected in the MATLAB structure?  
+1. If a reaction is properly removed or added to the model, which fields should be affected in the MATLAB structure?
 
+## Keeping track of model changes with GitHub
+One of the main pitfalls of introducing changes to GEMs on the EXCEL format is that this procedure doesn't leave a trace in which all the modifications can be seen and one can easily end up with multiple model files, which could be inconvenient for colaborative or long-term projects.
 
+For this purpose, the use of GitHub and the SBML format are suggested as a standard practice in nowadays GEMs community. The creation and modification of this kind of files can be done through MATLAB commands, which makes it even more attractive because extensive tasks can be automatized.
+
+1. Lets introduce a modification in our original model. Octanoic acid can be produced from cytosolic octanoyl-CoA through the expression of a heterologous thioesterase. A transport reaction from the extracellular compartment and an exchange reaction should be added for the cell to be able to secrete the product.
+
+1. Export the model to SBML format and commit the changes in GitHub desktop
 
--- CHASSY_ecModels/scripts/Exercise1.m
+++ CHASSY_ecModels/scripts/Exercise1.m
@@ -1,25 +1,70 @@
-%************* CHASSY modelling tutorial: Excercise #1 ********************
-
-% 1.1) Import the model from SBML format to a matlab structure. 
-model = importModel('yeast_7.6_cobra.xml','true','false');
+%%************* CHASSY modelling tutorial: Excercise #1 ********************
+% 1.1) Load the model MATLAB structure
+load('../models/GEMs/yeast_7_6.mat')
 % 2)  Export the model to an EXCEL file to get a comprenhensible overview
 %     of the contained information
-exportToExcelFormat(model,'yeast_7_6.xlsx');
-% 2a) How many rxns, mets, genes, comps are there in the model?
+exportToExcelFormat(model,'../models/GEMs/yeast_7_6.xlsx');
+% 3a) How many rxns, mets, genes, comps are there in the model?
 disp(model)
 % For a more detailed output:
 printModelStats(model,false,false);
 disp(['Number of Compartments: ' num2str(length(model.comps))])
-% 2b) How many rxns are reversible?
+fprintf('\n')
+% 3b) How many rxns are reversible?
 disp(['Number of reversible rxns: ' num2str(length(find(model.rev)))])
-% 2c) In how many rxns is the gene YEL039C present?
+fprintf('\n')
+% 3c) In how many rxns is the gene YEL039C present?
 YEL039C      = find(strcmpi(model.genes,'YEL039C'));
 nRxnsForGene = sum(model.rxnGeneMat(:,YEL039C));
 disp(['Gene YEL039C is present in ' num2str(nRxnsForGene) ' reactions'])
-% 2d) Gene associations for r_0195 and r_0198
+fprintf('\n')
+% 3d) Gene associations for r_0195 and r_0198
 rxnPos = strcmpi(model.rxns,'r_0195');
 disp(['Gene association for r_0195: ' model.grRules{rxnPos}])
+fprintf('\n')
 rxnPos = strcmpi(model.rxns,'r_0198');
 disp(['Gene association for r_0198: ' model.grRules{rxnPos}])
-% 2e) r_0438 equation
+fprintf('\n')
+% 3e) r_0438 equation
 printModel(model,'r_0438')
+fprintf('\n')
+%% Keeping track of GEMs modifications with GitHub
+
+% 5) Lets introduce a heterologous set of reactions for the production of
+% octanoic acid
+
+% Define reactions equations
+synthesisRxn = 'H2O[p] + octanoyl-CoA[p] => octanoic acid[p] + acetyl-CoA[p]';
+transportP   = 'octanoic acid[p] => octanoic acid[c]';
+transportC   = 'octanoic acid[c] => octanoic acid[e]';
+exchangeRxn  = 'octanoic acid[e] => ';
+
+rxnsToAdd.equations = {synthesisRxn; transportP; transportC; exchangeRxn}; 
+% Define reactions name
+synthesisRxn = 'octanoic acid synthesis';
+transportP   = 'octanoic acid transport p';
+transportc   = 'octanoic acid transport c';
+exchangeRxn  = 'octanoic acid exchange';
+rxnsToAdd.rxns     = {synthesisRxn; transportP; transportC; exchangeRxn};
+rxnsToAdd.rxnNames = rxnsToAdd.rxns;
+% Define objective and bounds
+rxnsToAdd.c  = [0 0 0 0];
+rxnsToAdd.lb = [0 0 0 0];
+rxnsToAdd.ub = [1000 1000 1000 1000];
+% Metabolites to Add
+metsToAdd.mets          = {'s_3717';'s_3718';'s_3719'};
+metsToAdd.metNames      = {'octanoic acid';'octanoic acid';'octanoic acid'};
+metsToAdd.compartments  = {'p';'c';'e'};
+% Introduce changes to the model
+octModel = addMets(model,metsToAdd);
+octModel = addRxns(octModel,rxnsToAdd,3);
+% Export to SBML format and save MATLAB structure of the new model
+current = pwd;
+cd ../models
+% Restore COBRA fields removed by addRxns
+octModel.rules = model.rules;
+saveModelSBML(octModel,'yeast_7_6',false)
+save('octAcidModel.mat','octModel')
+cd (current)
+clear
+% Check changes in GitHub desktop
