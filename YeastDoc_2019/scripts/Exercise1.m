908b6279bfe6cad39d6be16720080424c0415c06
Ivan Domenzain <ivand@chalmers.se>
2019-09-23 22:30:13 +0200

    chore: update exercise1 with 3HP production in yeastGEM

--- YeastDoc_2019/scripts/Exercise1.m
+++ YeastDoc_2019/scripts/Exercise1.m
@@ -1,29 +1,37 @@
-%%************* CHASSY modelling tutorial: Excercise #1 ********************
-% 1.1) Load the model MATLAB structure
-load('../models/GEMs/yeast_7_6.mat')
-% 2)  Export the model to an EXCEL file to get a comprenhensible overview
-%     of the contained information
-exportToExcelFormat(model,'../models/GEMs/yeast_7_6.xlsx');
+%YeastDoc modelling tutorial: Excercise #1 
+% Exercise1
+%
+%  Model structure exploration and modification. based on the latest
+%  version of yeastGEM (GitHub: https://github.com/SysBioChalmers/yeast-GEM)
+%
+% Ivan Domenzain.	Last modified 2019-09-23
+%
+
+% 3) Load the model MATLAB structure
+load('../models/yeastGEM.mat')
 % 3a) How many rxns, mets, genes, comps are there in the model?
 disp(model)
 % For a more detailed output:
 printModelStats(model,false,false);
-disp(['Number of Compartments: ' num2str(length(model.comps))])
-fprintf('\n')
 % 3b) How many rxns are reversible?
+%First approach, using the REV field
 disp(['Number of reversible rxns: ' num2str(length(find(model.rev)))])
+fprintf('\n')
+%Second approach, using the LB values
+disp(['Number of reversible rxns: ' num2str(length(find(model.lb<0)))])
 fprintf('\n')
 % 3c) In how many rxns is the gene YEL039C present?
 YEL039C      = find(strcmpi(model.genes,'YEL039C'));
 nRxnsForGene = sum(model.rxnGeneMat(:,YEL039C));
-disp(['Gene YEL039C is present in ' num2str(nRxnsForGene) ' reactions'])
+shortName    = model.geneShortNames{YEL039C};
+disp(['Gene YEL039C (' shortName ') is present in ' num2str(nRxnsForGene) ' reactions'])
 fprintf('\n')
 % 3d) Gene associations for r_0195 and r_0198
-rxnPos = strcmpi(model.rxns,'r_0195');
-disp(['Gene association for r_0195: ' model.grRules{rxnPos}])
+rxnPos = strcmpi(model.rxns,'r_0109');
+disp(['Gene association for r_0109: ' model.grRules{rxnPos}])
 fprintf('\n')
-rxnPos = strcmpi(model.rxns,'r_0198');
-disp(['Gene association for r_0198: ' model.grRules{rxnPos}])
+rxnPos = strcmpi(model.rxns,'r_0112');
+disp(['Gene association for r_0112: ' model.grRules{rxnPos}])
 fprintf('\n')
 % 3e) r_0438 equation
 printModel(model,'r_0438')
@@ -34,37 +42,42 @@ fprintf('\n')
 % octanoic acid
 
 % Define reactions equations
-synthesisRxn = 'H2O[p] + octanoyl-CoA[p] => octanoic acid[p] + acetyl-CoA[p]';
-transportP   = 'octanoic acid[p] => octanoic acid[c]';
-transportC   = 'octanoic acid[c] => octanoic acid[e]';
-exchangeRxn  = 'octanoic acid[e] => ';
-
-rxnsToAdd.equations = {synthesisRxn; transportP; transportC; exchangeRxn}; 
-% Define reactions name
-synthesisRxn = 'octanoic acid synthesis';
-transportP   = 'octanoic acid transport p';
-transportc   = 'octanoic acid transport c';
-exchangeRxn  = 'octanoic acid exchange';
-rxnsToAdd.rxns     = {synthesisRxn; transportP; transportC; exchangeRxn};
+MCR_rxn        = 'malonyl-CoA[c] + 2 NADPH[c] => 3-hydroxypropionic acid[c] + 2 NADP(+)[c]';
+transport_3HP  = '3-hydroxypropionic acid[c] => 3-hydroxypropionic acid[e]';
+exchange_3HP   = '3-hydroxypropionic acid[e] => ';
+rxnsToAdd.equations = {MCR_rxn; transport_3HP; exchange_3HP}; 
+% Define reaction names
+MCR_rxn       = '3HP synthesis';
+transport_3HP = '3HP transport';
+exchange_3HP  = '3HP exchange';
+rxnsToAdd.rxns     = {MCR_rxn; transport_3HP; exchange_3HP};
 rxnsToAdd.rxnNames = rxnsToAdd.rxns;
 % Define objective and bounds
-rxnsToAdd.c  = [0 0 0 0];
-rxnsToAdd.lb = [0 0 0 0];
-rxnsToAdd.ub = [1000 1000 1000 1000];
+rxnsToAdd.c  = [0 0 0];
+rxnsToAdd.lb = [0 0 0];
+rxnsToAdd.ub = [1000 1000 1000];
+
 % Metabolites to Add
-metsToAdd.mets          = {'s_3717';'s_3718';'s_3719'};
-metsToAdd.metNames      = {'octanoic acid';'octanoic acid';'octanoic acid'};
-metsToAdd.compartments  = {'p';'c';'e'};
+metsToAdd.mets          = {'s_3HP_c' 's_3HP_e'};
+metsToAdd.metNames      = {'3-hydroxypropionic acid' '3-hydroxypropionic acid'};
+metsToAdd.compartments  = {'c' 'e'};
+
+%genes to add
+genesToAdd.genes          = {'MCR'};
+genesToAdd.geneShortNames = {'MCR'};
+rxnsToAdd.grRules         = {'MCR' '' ''};
 % Introduce changes to the model
-octModel = addMets(model,metsToAdd);
-octModel = addRxns(octModel,rxnsToAdd,3);
+model_3HP = addGenesRaven(model,genesToAdd);
+model_3HP = addMets(model_3HP,metsToAdd);
+model_3HP = addRxns(model_3HP,rxnsToAdd,3);
+%Standardize gene related fields
+[grRules, rxnGeneMat] = standardizeGrRules(model_3HP,true);
+model_3HP.grRules     = grRules;
+model_3HP.rxnGeneMat  = rxnGeneMat;
+
 % Export to SBML format and save MATLAB structure of the new model
-current = pwd;
-cd ../models
-% Restore COBRA fields removed by addRxns
-octModel.rules = model.rules;
-saveModelSBML(octModel,'yeast_7_6',false)
-save('octAcidModel.mat','octModel')
+exportModel(model_3HP,'../models/yeastGEM.xml')
+save('../models/model_3HP.mat',' model_3HP')
 cd (current)
 clear
 % Check changes in GitHub desktop
