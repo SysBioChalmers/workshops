e650b39930677e15e76cb23b1c346eb7b2dd5601
Ivan Domenzain <ivand@chalmers.se>
2020-02-14 17:59:23 +0100

    fix: add generation of more condition dependent models and enzyme usages analysis across conditions

--- Tartu_2020/scripts/Exercise5_proteomics_Integration.m
+++ Tartu_2020/scripts/Exercise5_proteomics_Integration.m
@@ -5,14 +5,15 @@
 %  growing on glucose, pH 5.5, 28 y 32 celsius
 %
 %
-% Ivan Domenzain.	Last modified 2020-02-12
+% Ivan Domenzain.	Last modified 2020-02-14
 
 %Load models 
 current = pwd;
-ecModel       = open('../models/ecYeastGEM.mat');
+ecModel = open('../models/ecYeastGEM.mat');
+ecModel = ecModel.ecModel;
 ecModel_batch = open('../models/ecYeastGEM_batch.mat');
-
-%% Clone the necessary repos:
+ecModel_batch = ecModel_batch.ecModel_batch;
+% Clone the necessary repos:
 
 %delete GECKO in case that a previous copy exists here
 if isfolder('GECKO') 
@@ -28,50 +29,73 @@ git('checkout feat-add_utilities')
 copyfile('../../data/abs_proteomics.txt','databases')
 copyfile('../../data/fermentationData.txt','databases')
 
-%% Incorporate proteomics
-grouping   = [3 3]; %Our dataset contains three replicates per condition
+% Incorporate proteomics
+grouping   = [3 3 3 3]; %Our dataset contains three replicates per condition
 flexFactor = 1.05;  %Allowable flexibilization factor for fixing glucose uptake rate
 %Use GECKo utilities for proteomics integration
 cd geckomat/utilities/integrate_proteomics
 generate_protModels(ecModel,grouping,'ecYeastGEM',flexFactor,ecModel_batch);
 cd (current)
+close all
 %Transfer output models to the models folder in the tutorial repo
 mkdir('../models/prot_constrained')
 copyfile('GECKO/models/prot_constrained/ecYeastGEM/**','../models/prot_constrained')
 %remove GECKO repository
 rmdir('GECKO','s')
-
-%% Analyze flux distributions and enzyme usages
 clc
-%Run differential analysis on the flux and enzyme usage level
-model_Std = load('../models/prot_constrained/ecYeastGEM_Std.mat');
-model_HiT = load('../models/prot_constrained/ecYeastGEM_HiT.mat');
-model_Std = model_Std.ecModelP;
-model_HiT = model_HiT.ecModelP;
+
+% Analyze flux distributions and enzyme usages
+%Create a table with all flux distributions for all condition-dependent
+%models
+conditions = {'Std';'HiT';'LpH';'Osm'};
 cd complementary
+[~,enzTable_abs,~] = compare_ecModels_fluxDist(conditions,'ecYeastProt');
+%Show absolute usages for all enzymes with a coefficient of variation greater or
+%equal to 1 across conditions
+usages = cell2mat(table2cell(enzTable_abs(:,2:5)));
+CV     = std(usages,0,2)./mean(usages,2);
+CV(isnan(CV)) = 0;
+[CV,sorted]= sort(CV,1,'descend');
+disp('Enzyme usages sorted by their coefficient of variation across conditions')
+disp(enzTable_abs(sorted(CV>=1),:))
+%Run differential analysis on the flux and enzyme usage level for each
+%stress condition compared to the reference
+%Load ecModel+proteomics for standard conditions
+ref = load('../../models/prot_constrained/ecYeastGEM_Std.mat');
+ref = ref.ecModelP;
 %The function diff_FluxDist_analysis performs the required analysis, in
 %order to get to know its expected inputs let's display its help window
 help diff_FluxDist_analysis
-%Retrieve the necessary inputs from the models
-bioRxn  = model_Std.rxns(find(strcmpi(model_Std.rxnNames,'growth')));
-Csource = 'D-glucose exchange (reversible)';
-Drate   = 0.1; %From experiments!
+%Set the values for the requested inputs
+bioRxn   = ref.rxns(find(strcmpi(ref.rxnNames,'growth')));
+Csource  = 'D-glucose exchange (reversible)';
+Drate    = 0.1; %From experiments!
 fileName = 'Std_vs_HiT.txt';
-resultStruct = diff_FluxDist_analysis(model_Std,model_HiT,'r_2111','D-glucose exchange (reversible)',0.099,'Std_vs_HiT.txt');
 
-% Show the top 20 "up-regulated" fluxes and enzymes
-temp = sortrows(resultStruct.rxns,5,'descend');
-disp('The top 20 upregulated reaction fluxes are: ')
-disp(temp(1:20,:))
-disp(' ')
-temp = sortrows(resultStruct.proteins,4,'descend');
-disp('The top 20 upregulated  enzymes are: ')
-disp(temp(1:20,:))
-% Show the top 20 "down-regulated" fluxes and enzymes
-temp = sortrows(resultStruct.rxns,5,'ascend');
-disp('The top 20 downregulated reaction fluxes are: ')
-disp(temp(1:20,:))
-disp(' ')
-temp = sortrows(resultStruct.proteins,4,'ascend');
-disp('The top 20 downregulated  enzymes are: ')
-disp(temp(1:20,:))
+
+for i=2:length(conditions)
+    cond = conditions{i};
+    condModel  = load(['../../models/prot_constrained/ecYeastGEM_' cond '.mat']);
+    condModel  = condModel.ecModelP;
+    outputName = ['Std_vs_' cond '.txt'];
+    resultStruct = diff_FluxDist_analysis(ref,condModel,bioRxn,Csource,Drate,'Std_vs_HiT.txt');
+    % Show the top 20 "up-regulated" fluxes and enzymes
+    temp = sortrows(resultStruct.rxns,5,'descend');
+    disp(['The top 10 upregulated reaction fluxes for ' cond ' are: '])
+    disp(temp(1:10,:))
+    disp(' ')
+    temp = sortrows(resultStruct.proteins,4,'descend');
+    disp(['The top 10 upregulated  enzymes for ' cond ' are: '])
+    disp(temp(1:10,:))
+    % Show the top 20 "down-regulated" fluxes and enzymes
+    temp = sortrows(resultStruct.rxns,5,'ascend');
+    disp(['The top 10 downregulated reaction fluxes for ' cond ' are: '])
+    disp(temp(1:10,:))
+    disp(' ')
+    temp = sortrows(resultStruct.proteins,4,'ascend');
+    disp(['The top 10 downregulated  enzymes for ' cond ' are: '])
+    disp(temp(1:10,:))
+end
+cd (current)
+
+
