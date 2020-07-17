a8e4929761ec63896f4373799e71d38e8e3126ae
Ivan Domenzain <ivand@chalmers.se>
2020-07-17 16:16:12 +0200

    fix: adapt exercise2 to C intermedia model

--- Tartu_2020/scripts/explore_Cintermedia.m
+++ Tartu_2020/scripts/explore_Cintermedia.m
@@ -11,28 +11,28 @@
 %%Save current folder path
 current = pwd;
 % Load model
-model = importModel('../models/Candida_intermedia.xml')
+model = importModel('../models/Candida_intermedia.xml');
 % 1) Set the objective function to growth pseudo-reaction
-growthRxn = model_3HP.rxns{strcmpi(model_3HP.rxnNames,'growth')};
-tempModel = setParam(model_3HP,'obj',growthRxn,1);
+growthRxn = model.rxns{strcmpi(model.rxnNames,'growth')};
+tempModel = setParam(model,'obj',growthRxn,1);
 % As an alternative way
 tempModel.c(:) = 0;
-growthIndex    = strcmpi(model_3HP.rxnNames,'growth');
+growthIndex    = strcmpi(model.rxnNames,'growth');
 tempModel.c(growthIndex) = 1;
 % Take a look into the model's biomass composition
-printModel(model_3HP,growthRxn)
+printModel(model,growthRxn)
 fprintf('\n')
 % Set glucose uptake rate
-glucIN    = model_3HP.rxns{strcmpi(model_3HP.rxnNames,'D-glucose exchange')};
-tempModel = setParam(model_3HP,'lb',glucIN,-1);
+glucIN    = model.rxns{strcmpi(model.rxnNames,'D-glucose exchange')};
+tempModel = setParam(model,'lb',glucIN,-1);
 % As an alternative way
-glucIndex = strcmpi(model_3HP.rxnNames,'D-glucose exchange');
+glucIndex = strcmpi(model.rxnNames,'D-glucose exchange');
 tempModel.lb(glucIndex) = -1;
 % run FBA
 solution = solveLP(tempModel);
 % Explore solution structure
 colNames        = {'reactions' 'fluxes' 'grRules'};
-solution_Fluxes = table(model_3HP.rxnNames,solution.x,model_3HP.grRules,'VariableNames',colNames);
+solution_Fluxes = table(model.rxnNames,solution.x,model.grRules,'VariableNames',colNames);
 
 % 2a) Display exchange fluxes
 printFluxes(tempModel,solution.x);
@@ -42,7 +42,7 @@ fprintf('\n')
 %%%%% For this you should take a look into the printFluxes function and
 %%%%% call it accordingly
 cutOffFlux = 1E-2;
-printFluxes(model_3HP,solution.x,false,cutOffFlux);
+printFluxes(model,solution.x,false,cutOffFlux);
 
 % comparing FBA and pFBA solutions
 
@@ -51,15 +51,15 @@ printFluxes(model_3HP,solution.x,false,cutOffFlux);
 solution_pFBA = solveLP(tempModel,1);
 % Explore solution structure
 colNames             = {'reactions' 'fluxes' 'grRules'};
-solution_pFBA_Fluxes = table(model_3HP.rxnNames,solution_pFBA.x,model_3HP.grRules,'VariableNames',colNames);
+solution_pFBA_Fluxes = table(tempModel.rxnNames,solution_pFBA.x,tempModel.grRules,'VariableNames',colNames);
 % 3a) Display exchange fluxes
-printFluxes(model_3HP,solution_pFBA.x);
+printFluxes(tempModel,solution_pFBA.x);
 fprintf('\n')
 % 3b) Display internal fluxes
 %For this you should take a look into the printFluxes function and
 %call it accordingly
 cutOffFlux = 1E-2;
-printFluxes(model_3HP,solution_pFBA.x,false,cutOffFlux);
+printFluxes(tempModel,solution_pFBA.x,false,cutOffFlux);
 %As you have seen there are many reactions that have -1000 or 1000 flux.
 %This is because there are loops in the solution. In order to clean up the
 %solution we can minimize the sum of all the fluxes. This is done by
@@ -71,10 +71,10 @@ printFluxes(model_3HP,solution_pFBA.x,false,cutOffFlux);
 %This will show a cumulative distribution of the absolute values of the
 %fluxes in the simulation
 cd complementary
-FluxDist = solution.x(abs(solution.x)>1E-7);
+FluxDist = solution.x;%(abs(solution.x)>1E-7);
 plot2D(abs(FluxDist),[],'FBA simulation','Fluxes value [mmol/gDCW h]','Cumulative distribution',true)
 %Look at the new flux cumulative distribution
-FluxDist = solution_pFBA.x(abs(solution_pFBA.x)>1E-7);
+FluxDist = solution_pFBA.x;%(abs(solution_pFBA.x)>1E-7);
 plot2D(abs(FluxDist),[],'pFBA simulation','Fluxes value [mmol/gDCW h]','Cumulative distribution',true)
 
 % Growth rate vs Glucose uptake rate
@@ -85,7 +85,7 @@ GURates = [];
 for i=1:20+1
     %Set new lb for glucose uptake at every iteration
     GUR = i-1;
-    tempModel = setParam(model_3HP,'lb',glucIN,-GUR);
+    tempModel = setParam(model,'lb',glucIN,-GUR);
     solution = solveLP(tempModel);
     %If the simulation was feasible then save the results
     if ~isempty(solution.f)
@@ -101,15 +101,15 @@ plot2D(GURates,gRates,'','GUR [mmol/gDw h]','Growth rate [g/gDCW h]',false)
 % 6) Compare flux distributions for growth on glucose and glycerol
 
 % First get the flux distribution on glucose
-glyIN         = model_3HP.rxns{strcmpi(model_3HP.rxnNames,'glycerol exchange')};
-tempModel     = setParam(model_3HP,'lb',{glucIN glyIN},[-1 0]);
-glucIndex     = find(strcmpi(model_3HP.rxns,glucIN));
-glycIndex     = find(strcmpi(model_3HP.rxns,glyIN));
-grwtIndex     = find(strcmpi(model_3HP.rxns,growthRxn));
+glyIN         = model.rxns{strcmpi(model.rxnNames,'glycerol exchange')};
+tempModel     = setParam(model,'lb',{glucIN glyIN},[-1 0]);
+glucIndex     = find(strcmpi(model.rxns,glucIN));
+glycIndex     = find(strcmpi(model.rxns,glyIN));
+grwtIndex     = find(strcmpi(model.rxns,growthRxn));
 solGluc       = solveLP(tempModel,1);
 bioYieldGluc  = solGluc.x(grwtIndex)/(solGluc.x(glucIndex)*0.18);
 % Get the flux distribution on glycerol
-tempModel     = setParam(model_3HP,'lb',{glucIN glyIN},[0 -1]);
+tempModel     = setParam(model,'lb',{glucIN glyIN},[0 -1]);
 solGly        = solveLP(tempModel,1);
 bioYieldGlyc  = solGly.x(grwtIndex)/(solGly.x(glycIndex)*0.09209);
 %Print results
@@ -128,58 +128,10 @@ fprintf('\n')
 %select which reactions to print. Here we show reactions that differ with
 %more than 50%, have a flux higher than 0.5 mmol/gDW/h and an absolute
 %difference higher than 0.5 mmol/gDW/h.
-followChanged(tempModel,solGluc.x,solGly.x, 50, 0.5, 0.5);
+followChanged(tempModel,solGly.x,solGluc.x, 50, 0.5, 0.5);
 fprintf('\n')
 %Say that we are particularly interested in how ATP metabolism
 %changes. Then we can show its related reactions by writing,
-followChanged(tempModel,solGluc.x,solGly.x, 30, 0.4, 0.4,{'ATP'});
+followChanged(tempModel,solGly.x,solGluc.x, 30, 0.4, 0.4,{'ATP'});
 fprintf('\n')
-
-
-% Exploring production yield for 3HP
-
-% 7) Get a yield vs gRate plot for 3-Hydroxypropionic acid
-BioYield    = [];
-yield       = [];
-Index3HP    = find(strcmpi(model_3HP.rxnNames,'3HP exchange'));
-iterations  = 10;
-Dmax        = 0.2;
-%Get a maximum GUR subject to Dmax
-tempModel = setParam(model_3HP,'lb',model_3HP.rxns(growthIndex),0.9999*Dmax);
-tempModel = minimal_Y6(tempModel,glucIN,-1000);
-tempModel = setParam(tempModel,'obj',glucIN,1);
-sol       = solveLP(tempModel,1);
-GURopt    = sol.x(glucIndex);
-
-for i=1:iterations+1
-    % Set the objective function to the added 3HP exchange rxn
-    tempModel = setParam(model_3HP,'obj',model_3HP.rxns{Index3HP},1);
-    % Set a minimal glucose media and GURopt as UB for GUR
-    tempModel = minimal_Y6(tempModel,glucIN,GURopt);
-    % Fix dilution rate at every iteration
-    Drate     = Dmax*(i-1)/iterations;
-    tempModel = setParam(tempModel,'lb',model_3HP.rxns(growthIndex),0.9999*Drate);
-    solution  = solveLP(tempModel);
-    if ~isempty(solution.f)
-        Exc_3HP  = solution.x(Index3HP);
-        % Fix maximal production rate for 3HP
-        tempModel = setParam(tempModel,'eq','3HP exchange',Exc_3HP);
-        % Fix objective for glucose uptake rate minimization
-        tempModel = setParam(tempModel,'obj',glucIN,1);
-        solution  = solveLP(tempModel,1);
-        GUR       = abs(solution.x(glucIndex));
-        % Calculate biomass yield (g biomass/g glucose)
-        value     = Drate/(GUR*0.18);
-        BioYield  = [BioYield; value];
-        % Calculate 3HP yield [mmol 3HP/mmol glucose]
-        value     = Exc_3HP*0.0908/(GUR*0.180);
-        yield     = [yield; value];
-    end
-end
-plot2D(BioYield,yield,'','Biomass yield [g biomass/g gluc]','3HP yield [g/g glucose]',false)
-cd (current)
-
-        
-        
-        
      
